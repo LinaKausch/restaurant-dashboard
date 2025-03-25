337bd1a0235933503ee6b4f255eb4954723f4348
Lina Kausch <lina.kausch@student.howest.be>
2025-03-25 11:11:47 +0100

    casing....

--- src/components/TableList.jsx
+++ src/components/TableList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Table from "./table";
+import Table from "./Table";
 
-function TableList({ tables, setTables }){
+function TableList({ tables, setTables }) {
 
   const assignWaiter = (name, tableNumber) => {
     setTables((prevTables) =>
@@ -13,29 +13,29 @@ function TableList({ tables, setTables }){
     );
   };
 
-    const updateNumPeople = (numPeople, tableNumber) => {
-        setTables((prevTables) =>
-            prevTables.map((table) =>
-                table.number === tableNumber
-                    ? { ...table, numPeople } // Update number of people for the correct table
-                    : table
-            )
-        );
-    };
-
-    return (
-        <div>
-            <h2>Tables List</h2>
-            {tables.map((table) => (
-                <Table
-                    key={table.number}
-                    table={table}
-                    assignWaiter={assignWaiter}
-                    updateNumPeople={updateNumPeople}
-                />
-            ))}
-        </div>
+  const updateNumPeople = (numPeople, tableNumber) => {
+    setTables((prevTables) =>
+      prevTables.map((table) =>
+        table.number === tableNumber
+          ? { ...table, numPeople } // Update number of people for the correct table
+          : table
+      )
     );
+  };
+
+  return (
+    <div>
+      <h2>Tables List</h2>
+      {tables.map((table) => (
+        <Table
+          key={table.number}
+          table={table}
+          assignWaiter={assignWaiter}
+          updateNumPeople={updateNumPeople}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default TableList;
