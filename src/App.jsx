fbc396ccd8ecda1dbb602737a5cc8d52af0d5580
Lina Kausch <lina.kausch@student.howest.be>
2025-03-29 22:51:44 +0100

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TableList from "./components/TableList";
 import Order from "./components/Order";
 import Menu from "./components/Menu";
